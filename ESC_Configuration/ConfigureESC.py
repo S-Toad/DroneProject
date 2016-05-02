9d202c505bb862621410e845120efc4198ce7ed1
SexyToad <SexyToad@AvatarMC.com>
2016-05-01 21:26:48 -0700

    Modified Configuration program to read from a configuration file :D

--- ESC_Configuration/ConfigureESC.py
+++ ESC_Configuration/ConfigureESC.py
@@ -1,26 +1,24 @@
 from Adafruit_PWM_Servo_Driver import PWM
 import time
+from ConfigParser import SafeConfigParser
 
 '''
 This code should be ran with any new ESC (Electronic Speed Controllers) to configure them to the correct parameters.
 Below are parameters that 'you' are able to change to fit with your ideal specifications.
 
-A typical ESC will take 20ms (50hz) pulses. In those pulses, 1 ms of being on will be considered the bottom range of your throttle, while 2 is considered the high end.
-These values are changeble.
-
-These are in Adafruits ticks, (0-4095). Where 1 tick is pulseHertz/4095
-For 50hz, 1ms is about ~205
-The default range will be from 200-400 (~0.98ms - ~1.95ms), where each tick is about +0.5% power. 
+Please see ESC_Configuration.INI for summary of configurations
 '''
-
-# These are configurations the user is able to change
-# ToDo: Have all configurations available in a seperate config file at root of repo
-pulseHertz = 50
-bottomTickRange = 200
-upperTickRange = 400
 
 # This takes the raw input from the user and sorts into a list of integers to use.
 channel_list = map(int,raw_input("Enter the ESC channel(s), (Example: 12 13 14 15): "))
+
+# This section simply reads from the ESC_Configuration.INI for the values needed
+# TODO: Make this into it's own method to keep things clean? 
+config = SafeConfigParser()
+config.read("ESC_Configuration.INI")
+pulseHertz = int(config.get('ESC_Config', 'pulseHertz'))
+bottomTickRange = int(config.get('ESC_Config', 'bottomTickRange'))
+upperTickRange = int(config.get('ESC_Config', 'upperTickRange'))
 
 # This sets the address and frequency the adafruit servo driver will use
 pwm = PWM(0x40)
@@ -36,6 +34,7 @@ for channel in channel_list:
     pwm.setPWM(channel, 0, bottomTickRange)
 raw_input("Hit any key after all the beeps...")
 # TODO: Use time.sleep() for ease^
+# TODO: Make each configuration it's own method/file?
 
 print "Will run at 5% for a short period"
 time.sleep(3)
