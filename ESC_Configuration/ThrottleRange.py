4ee20034a751be08c408e03d9c9aeac20a285d39
SexyToad <SexyToad@AvatarMC.com>
2016-05-01 21:14:35 -0700

    Altered the configuration program to accept multiple channels at once.

--- ESC_Configuration/ThrottleRange.py
+++ ESC_Configuration/ThrottleRange.py
@@ -1,13 +1,9 @@
-import sys
-sys.path.append('../Adafruit-Raspberry-Pi-Python-Code/Adafruit_PWM_Servo_Driver/')
-
 from Adafruit_PWM_Servo_Driver import PWM
 import time
 
 '''
 This code should be ran with any new ESC (Electronic Speed Controllers) to configure them to the correct parameters.
 Below are parameters that 'you' are able to change to fit with your ideal specifications.
-Default settings
 
 A typical ESC will take 20ms (50hz) pulses. In those pulses, 1 ms of being on will be considered the bottom range of your throttle, while 2 is considered the high end.
 These values are changeble.
@@ -17,26 +13,35 @@ For 50hz, 1ms is about ~205
 The default range will be from 200-400 (~0.98ms - ~1.95ms), where each tick is about +0.5% power. 
 '''
 
+# These are configurations the user is able to change
+# ToDo: Have all configurations available in a seperate config file at root of repo
 pulseHertz = 50
 bottomTickRange = 200
 upperTickRange = 400
 
-channel = raw_input("Enter the ESC channel: ")
+# This takes the raw input from the user and sorts into a list of integers to use.
+channel_list = map(int,raw_input("Enter the ESC channel(s), (Example: 12 13 14 15): "))
 
+# This sets the address and frequency the adafruit servo driver will use
 pwm = PWM(0x40)
 pwm.setPWMFreq(pulseHertz)
 
-pwm.setPWM(channel, 0, upperTickRange)
-print "Running high throttle..."
+# We iterate through each of the channels and set them to run at the upper range
+for channel in channel_list:
+    pwm.setPWM(channel, 0, upperTickRange)
 raw_input("Connect battery and hit any key after hearing 2 beeps...")
 
-pwm.setPWM(channel, 0, bottomTickRange)
-print "Running low throttle..."
+# We iterate through each of the channels and set them to run at the lower range
+for channel in channel_list:
+    pwm.setPWM(channel, 0, bottomTickRange)
 raw_input("Hit any key after all the beeps...")
 
-print "Running at 50% for a short period"
-pwm.setPWM(channel, 0, 300)
+print "Will run at 5% for a short period"
 time.sleep(3)
 
+for channel in channel_list:
+    pwm.setPWM(channel, 0, 210)
+time.sleep(3)
+pwm.setPWMFreq(0)
 
 print "The ESC on channel " + str(channel) + " is set!"
