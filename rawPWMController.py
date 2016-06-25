98ffcc2cacb41f5932b4b1e2cfc827ffa40e078d
S-Toad (Alex) <alexayala9975@hotmail.com>
2016-06-24 21:38:58 -0700

    Create an instance of the PWM_Handler class

--- rawPWMController.py
+++ rawPWMController.py
@@ -1,15 +1,14 @@
 from Handlers.PWM_Handler import PWM_Handler
 
-PWM_Handler.intializePWMFreq()
-
+pwmHandler = PWM_Handler.intializePWMFreq()
 channel = input("Enter PWM channel: ")
 
 while (True):
     power = input("Enter power or q to quit: ")
     
     if power == 'q':
-        PWM_Handler.turnOffChannel(channel)
+        pwmHandler.turnOffChannel(channel)
         break
     else:
         power = float(power)
-        PWM_Handler.setPower(channel, power)
+        pwmHandler.setPower(channel, power)
