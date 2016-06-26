78627682f552b2d7ec27430f31bbe996c1cfd1ff
S-Toad (Alex) <alexayala9975@hotmail.com>
2016-06-26 01:10:21 -0700

    Using class methods now

--- Handlers/PWM_Handler.py
+++ Handlers/PWM_Handler.py
@@ -3,31 +3,35 @@ from ESC_Configuration_Handler import ESC_Configuration_Handler
 
 class PWM_Handler():
     
+    @classmethod
     def getPWM(self):
         pwm = Adafruit_PCA9685.PCA9685()
         return pwm
-    
+        
+    @classmethod
     def intializePWMFreq(self):
-        pwm = PWM_Handler.getPWM()
         freq = ESCConfigurationHandler.returnPulseHertz()    
+        self.setPWMFreq(freq)
+    
+    @classmethod
+    def setPWMFreq(self, freq):
+        pwm = self.getPWM()   
         pwm.setPWMFreq(freq)
     
-    def setPWMFreq(freq):
-        pwm = PWM_Handler.getPWM()   
-        pwm.setPWMFreq(freq)
-    
+    @classmethod
     def setPower(self, channel, percentPower):
-        pwm = PWM_Handler.getPWM()
+        pwm = self.getPWM()
         tick = round(percentPower * 2)
         pwm.setPWM(channel, 0, tick)
     
+    @classmethod
     def turnOffChannel(self, channel):
-        pwm = PWM_Handler.getPWM()
+        pwm = self.getPWM()
         pwm.setPWM(channel, 0, 0)
     
+    @classmethod
     def turnOffAllChannels(self):
-        pwm = PWM_Handler.getPWM()
         for channel in range (0, 16):
-            PWM_Handler.turnOffChannel(channel)
+            self.turnOffChannel(channel)
         
-        PWM_Handler.setPWMFreq(0)
+        self.setPWMFreq(0)
--- rawPWMController.py
+++ rawPWMController.py
@@ -1,14 +1,13 @@
 from Handlers.PWM_Handler import PWM_Handler
 
-pwmHandler = PWM_Handler()
 channel = input("Enter PWM channel: ")
 
 while (True):
     power = input("Enter power or q to quit: ")
     
     if power == 'q':
-        pwmHandler.turnOffChannel(channel)
+        PWM_Handler.turnOffChannel(channel)
         break
     else:
         power = float(power)
-        pwmHandler.setPower(channel, power)
+        PWM_Handler.setPower(channel, power)
