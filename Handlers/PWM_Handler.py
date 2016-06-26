6a3d923d403a130108f37d9ce453b2158e11114c
S-Toad (Alex) <alexayala9975@hotmail.com>
2016-06-26 01:37:39 -0700

    Added debugging messages to PWM_Handler

--- Handlers/PWM_Handler.py
+++ Handlers/PWM_Handler.py
@@ -5,6 +5,8 @@ from time import sleep
 #TODO: Make an instance of this class and pass it around?
 
 class PWM_Handler():
+
+    debug = false
     
     @classmethod
     def getPWM(self):
@@ -15,22 +17,35 @@ class PWM_Handler():
     def intializePWMFreq(self):
         freq = ESCConfigurationHandler.returnPulseHertz()
         self.setPWMFreq(freq)
+        
+        if debug:
+            print("PWM frequency is set at: " + freq)
+            
     
     @classmethod
     def setPWMFreq(self, freq):
         pwm = self.getPWM()
         pwm.set_pwm_freq(freq)
+        
+        if debug:
+            print("PWM frequency is set at " + freq + " Hz.")
     
     @classmethod
     def setPower(self, channel, percentPower):
         pwm = self.getPWM()
         tick = int(round(percentPower * 2))
         pwm.set_pwm(channel, 0, tick)
+        
+        if debug:
+            print("Channel " + channel + " was set at " + percentPower + ".")
     
     @classmethod
     def turnOffChannel(self, channel):
         pwm = self.getPWM()
         pwm.set_pwm(channel, 0, 0)
+        
+        if debug:
+            print("Channel " + channel + " was turned off.")
     
     @classmethod
     def turnOff(self):
@@ -38,3 +53,6 @@ class PWM_Handler():
         pwm.set_all_pwm(0, 0)
         sleep(0.5)
         self.setPWMFreq(0)
+        
+        if debug:
+            print("PWM Servo Driver was turned off".)
