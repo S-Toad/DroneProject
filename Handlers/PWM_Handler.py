0a553c5d62654bd289496854f6935228f370f4bc
S-Toad (Alex) <alexayala9975@hotmail.com>
2016-06-28 01:17:36 -0700

    Temp removed debug messaged (want global way to do this) Reworked to take the ESCconfig object to pull the hertz Converted to an instance object as well.

--- Handlers/PWM_Handler.py
+++ Handlers/PWM_Handler.py
@@ -2,57 +2,27 @@ import Adafruit_PCA9685
 from ESC_Configuration_Handler import ESC_Configuration_Handler
 from time import sleep
 
-#TODO: Make an instance of this class and pass it around?
-
-class PWM_Handler():
-
-    debug = False
+class PWMHandler():
     
-    @classmethod
-    def getPWM(self):
-        pwm = Adafruit_PCA9685.PCA9685()
-        return pwm
+    def __init__(self, escConfigHandler):
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.escConfigHandler = escConfigHandler
         
-    @classmethod
     def intializePWMFreq(self):
-        freq = ESC_Configuration_Handler.returnPulseHertz()
+        freq = self.escConfigHandler.returnPulseHertz()
         self.setPWMFreq(freq)
-        
-        if debug:
-            print("PWM frequency is set at: " + freq)
             
-    
-    @classmethod
     def setPWMFreq(self, freq):
-        pwm = self.getPWM()
-        pwm.set_pwm_freq(freq)
-        
-        if debug:
-            print("PWM frequency is set at " + freq + " Hz.")
+        self.pwm.set_pwm_freq(freq)
     
-    @classmethod
     def setPower(self, channel, percentPower):
-        pwm = self.getPWM()
         tick = int(round(percentPower * 2))
-        pwm.set_pwm(channel, 0, tick)
-        
-        if debug:
-            print("Channel " + channel + " was set at " + percentPower + ".")
+        self.pwm.set_pwm(channel, 0, tick)
     
-    @classmethod
     def turnOffChannel(self, channel):
-        pwm = self.getPWM()
-        pwm.set_pwm(channel, 0, 0)
-        
-        if debug:
-            print("Channel " + channel + " was turned off.")
+        self.pwm.set_pwm(channel, 0, 0)
     
-    @classmethod
     def turnOff(self):
-        pwm = self.getPWM()
-        pwm.set_all_pwm(0, 0)
+        self.pwm.set_all_pwm(0, 0)
         sleep(0.5)
         self.setPWMFreq(0)
-        
-        if debug:
-            print("PWM Servo Driver was turned off.")
