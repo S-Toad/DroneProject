f95b6818d9a20a6b30bd29f7ba825792783a47b5
Alex Ayala (S-Toad) <AlexAyala9975@hotmail.com>
2016-07-18 01:16:45 -0700

    Added Vector class to handle simple x, y, z numbers. This will house more complex functions in the future

---
+++ Math/Vector.py
@@ -0,0 +1,18 @@
+
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+    def getZ(self):
+        return self.z
+    
+    def printVector(self):
+        print(str(self.x) + ', ' + str(self.y) + ', ' + str(self.z))
