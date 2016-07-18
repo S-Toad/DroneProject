1e3022d9f877bad684d44056efdea357ec5ea613
Alex Ayala (S-Toad) <AlexAyala9975@hotmail.com>
2016-07-18 01:19:05 -0700

    ConfigHandler can now reload itself and adjust the current section. This is for fast data grabbing

--- Configuration/ConfigHandler.py
+++ Configuration/ConfigHandler.py
@@ -1,13 +1,25 @@
 import configparser
 
-class configHandler:
-    
-    def __init__(self, fileName):
+class ConfigHandler:
+    def __init__(self, fileName, section):
         self.fileName = fileName
-        self.config = configparser.ConfigParser()
-        self.config.read(self.fileName)
+        self.section = section
+        self.reload()
     
-    def getConfig(self, section, option):
+    def getSectionData(self, section, option):
         optionValue = self.config.get(section, option)
         return optionValue
     
+    def getData(self, option):
+        optionValue = self.config.get(self.section, option)
+        return optionValue
+        
+    def setSection(self, section):
+        self.section = section
+    
+    def hasSection(self, section):
+        return self.config.has_section(section)
+    
+    def reload(self):
+        self.config = configparser.ConfigParser()
+        self.config.read(self.fileName)
